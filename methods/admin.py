709f3839e2486e25c2c1672495f4c55ab3569d9d
Priyank Trivedi <priyankt68@gmail.com>
2016-10-29 14:08:35 +0530

    Adding unicode methods domain and tbl_scope with improved admin display for definitions.

--- methods/admin.py
+++ methods/admin.py
@@ -2,7 +2,24 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Definitions)
+
+class DefinitionsAdmin(admin.ModelAdmin):
+    """
+
+    """
+
+    list_display = ('name', 'definition')
+    list_filter = ('domain', 'tbl_scope')
+    fieldsets = (
+        (None, {'fields':('name', 'definition')}),
+        ('Link', {'fields': ('link',)}),
+        ('TBL Scope', {'fields': ('tbl_scope',)}),
+        ('Domain', {'fields': ('domain',)}),
+        ('Remarks', {'fields': ('remarks',)}))
+
+    search_fields = ('name','domain', 'tbl_scope')
+
+
+admin.site.register(Definitions, DefinitionsAdmin)
 admin.site.register(Domain)
 admin.site.register(TBL_Scope)
-
--- methods/models.py
+++ methods/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class TBL_Scope(models.Model):
     """
 
@@ -7,12 +8,27 @@ class TBL_Scope(models.Model):
 
     tbl_scope = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        """
+
+        :return:
+        """
+        return self.tbl_scope
+
+
 class Domain(models.Model):
     """
 
     """
 
     domain = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        """
+
+        :return:
+        """
+        return self.domain
 
 
 class Definitions(models.Model):
@@ -22,10 +38,10 @@ class Definitions(models.Model):
 
     name = models.CharField(max_length=255)
     definition = models.TextField()
-    link = models.CharField(max_length=255)
+    link = models.CharField(max_length=255, null=True)
     tbl_scope = models.ManyToManyField(TBL_Scope, blank=True)
     domain = models.ManyToManyField(Domain, blank=True)
-    remarks = models.TextField()
+    remarks = models.TextField(null=True)
 
 
 
