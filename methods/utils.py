c432934df7b507f9b54353af23b9c5f966822a22
Priyank Trivedi <priyankt68@gmail.com>
2016-12-01 10:48:29 +0530

    Adding util method to get indicator type.

--- methods/utils.py
+++ methods/utils.py
@@ -9,21 +9,31 @@ def get_definitions(domain, tbl_scope):
     :param tbl_scope:
     :return:
     """
-
-
     definitions = Definitions.objects.filter(tbl_scope__tbl_scope=tbl_scope, domain__domain=domain)
-
     return definitions
 
-def get_indicators( tbl_scope):
+
+def get_indicators(tbl_scope):
     """
-    Based on :problem, :domain and :tbl_scope, fetch all matching definitions.
+    Based on :tbl_scope, fetch all matching indicators.
     :param problem:
     :param domain:
     :param tbl_scope:
     :return:
     """
-
-
     indicators = Indicators.objects.filter(tbl_scope__tbl_scope=tbl_scope)
     return indicators
+
+
+def get_indicator_type(tbl_scope):
+    """
+    Based on :tbl_scope, fetch all matching indicators.
+    :param problem:
+    :param domain:
+    :param tbl_scope:
+    :return:
+    """
+    indicators = Indicators.objects.filter(tbl_scope__tbl_scope=tbl_scope)
+    types = [indicator.type for indicator in indicators]
+
+    return set(types)
